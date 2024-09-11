c1e47e2dc753aa1bbeed8622ab9b0a0f920fcd3f
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:25:04 +0200

    [UPD] functions added

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function randomPassword($length)
+{
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_";
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[mt_rand(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
