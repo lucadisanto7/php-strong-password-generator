f34e2130d77edc9dfa8a3775e26d2695623fa785
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:26:04 +0200

    [UPD] Added html

--- result.php
+++ result.php
@@ -8,3 +8,18 @@ if (!isset($_SESSION['generatedPassword'])) {
 
 $generatedPassword = $_SESSION['generatedPassword'];
 ?>
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title></title>
+</head>
+
+<body>
+    
+</body>
+
+</html>
